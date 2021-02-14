d01100b50180d579b3886d20b58ddf67525dc1a8
scorsaro <salvatore.corsaro@live.com>
2021-02-14 23:59:34 +0100

    Almost 100%

--- src/test/java/com/scorsaro/bbas/controller/impl/AccountHolderControllerTest.java
+++ src/test/java/com/scorsaro/bbas/controller/impl/AccountHolderControllerTest.java
@@ -17,12 +17,17 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import java.time.LocalDate;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 class AccountHolderControllerTest {
@@ -47,7 +52,7 @@ class AccountHolderControllerTest {
         PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
         AccountHolder accountHolder = new AccountHolder("pepito", passwordEncoder.encode("pepito"),
-                new Name("Pepe", "Lulo", "Dominguez"), LocalDate.of(1999, 12, 12),
+                new Name("Luca", "Lulo", "Dominguez"), LocalDate.of(1999, 12, 12),
                 new Address("Madrid", "España", "Ancora 6", "28045"));
 
         Role role = new Role("ACCOUNT_HOLDER", accountHolder);
@@ -64,7 +69,10 @@ class AccountHolderControllerTest {
     }
 
     @Test
-    void getAll() {
+    void getAll() throws Exception {
+        MvcResult result = mockMvc.perform(get("/account-holder")
+                .with(user("admin").roles("ADMIN"))).andExpect(status().isOk()).andReturn();
+        assertTrue(result.getResponse().getContentAsString().contains("Luca"));
     }
 
     @Test
